56b8106c4e2628e377c1a2aae6be122c8cccdce4
folone <folone@gmail.com>
2013-02-24 21:34:16 +0100

    Some arrows to make it more interesting.

--- Words.scala
+++ Words.scala
@@ -1,29 +1,43 @@
 import scala.io.Source
 // Using scalaz a-la-carte imports to not polute the namespace
 import scalaz._
+import std.function._
+import syntax.arrow._
 import syntax.show._
 import effect._
 import IO._
 
 object Words {
   // :: Char → Boolean
-  def letterOrDigitOrWhitespace(c: Char) = c.isLetterOrDigit || c.isWhitespace
+  def acceptedChars(c: Char) = {
+    // We could, of course, do
+    //c.isLetterOrDigit || c.isWhitespace || c == '-'
+    // But that would have been boring.
+    // How about some Arrows?
+    val fun = ((c: Char) ⇒ c.isLetterOrDigit) &&&
+              ((c: Char) ⇒ c.isWhitespace)    &&&
+              ((c: Char) ⇒ c == '-')
+    fun(c) match {
+      case((a, b), c) ⇒ a || b || c
+    }
+  }
 
   // Show typeclass instance
   // instance Show List (String, Int) where
   implicit val mapShow = new Show[List[(String, Int)]] {
-    override def shows(l: List[(String, Int)]) = l.foldLeft("") { case(acc, (key, value)) ⇒
-        acc + "\n" + key + ": " + value
-    }
+    override def shows(l: List[(String, Int)]) =
+      l.foldLeft("") { case(acc, (key, value)) ⇒
+          acc + "\n" + key + ": " + value
+      }
   }
 
   // :: String → List (String, Int)
   def wordCount(text: String): List[(String, Int)] =
         // Leave letters, digits and spaces
-    text.filter(letterOrDigitOrWhitespace)
+    text.filter(acceptedChars)
         // split words
         .toLowerCase.split("\\W")
-        // Oprionally parallelize
+        // Optionally parallelize
         .par
         // group
         .groupBy(identity)
@@ -36,12 +50,12 @@ object Words {
         .sortBy { case(_, value) ⇒ -value }
         // take the largest 10
         .take(10)
-        // Unparallelize
+        // Get results from parallel computation
         .seq.toList
 
   // :: String → IO String
   def getFileContent(path: String) = IO {
-    val source = Source.fromFile(path)
+    val source  = Source.fromFile(path)
     val content = source.mkString
     source.close()
     content
@@ -49,11 +63,13 @@ object Words {
 
   // :: Array String → ()
   def main(args: Array[String]) = {
-    val path = args(0)
+    val path   = args(0)
     val action = for {
-      text ← getFileContent(path)
+      text   ← getFileContent(path)
+      // Don't need timing? Just do:
+      // result = wordCount(text)
       result ← time(wordCount(text))
-      _ ← putStrLn(result.shows)
+      _      ← putStrLn(result.shows)
     } yield ()
     // Yuck!
     action.unsafePerformIO()
@@ -62,9 +78,9 @@ object Words {
   // Profiling function
   def time[R](block: ⇒ R): IO[R] =
     for {
-      t0 ← IO { System.nanoTime() }
+      t0     ← IO { System.nanoTime() }
       result = block
-      t1 ← IO { System.nanoTime() }
-      _ ← putStrLn("Elapsed time: " + (t1 - t0) + " ns")
+      t1     ← IO { System.nanoTime() }
+      _      ← putStrLn("Elapsed time: " + (t1 - t0) + " ns")
     } yield result
 }
