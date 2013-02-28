7d761b8037de5bd8d878de9cba15af4f53aeb58f
folone <folone@gmail.com>
2013-02-28 17:59:14 +0100

    Use Monoid instance for Monoid b => a → b

--- src/main/scala/info.folone.words/Main.scala
+++ src/main/scala/info.folone.words/Main.scala
@@ -5,10 +5,10 @@ import scalaz._, Scalaz._
 object Main {
   def main(args: Array[String]) {
     val path = args(0)
-    val action = WordsMemory.mainIO(path) |+|
-                 WordsStream.mainIO(path) |+|
-                 WordMachine.mainIO(path)
+    val action = WordsMemory.mainIO _ |+|
+                 WordsStream.mainIO _ |+|
+                 WordMachine.mainIO _
     // Yuck!
-    action.unsafePerformIO()
+    action(path).unsafePerformIO()
   }
 }
