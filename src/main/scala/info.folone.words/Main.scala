948a17791a3885d56962e3ec31921b23c91c6848
folone <folone@gmail.com>
2013-04-16 14:20:06 +0200

    Preparing the code for the presentation

--- src/main/scala/info.folone.words/Main.scala
+++ src/main/scala/info.folone.words/Main.scala
@@ -4,11 +4,18 @@ import scalaz._, Scalaz._
 
 object Main {
   def main(args: Array[String]) {
-    val path = args(0)
-    val action = WordsMemory.mainIO _ |+|
-                 WordsStream.mainIO _ |+|
-                 WordMachine.mainIO _
+    val path     = args(0)
+    val actions  = List(WordsMemory.mainIO _, WordsStream.mainIO _, WordMachine.mainIO _)
+    val action   = actions.suml // use the Monoid instance, foldl(IO())(_ |+| _)
+    val results  = actions.traverse(f ⇒ f(path)) // Use the Applicative instance (sequence . map)
+    val results2 = for {
+      _ ← WordsMemory.mainIO(path)
+      _ ← WordsStream.mainIO(path)
+      _ ← WordsStream.mainIO(path)
+    } yield () // Use the Monad instance
     // Yuck!
     action(path).unsafePerformIO()
+    results.unsafePerformIO()
+    results2.unsafePerformIO()
   }
 }
