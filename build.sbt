3471fd1be33edbba5f9ea98cc9751571548b2804
folone <folone@gmail.com>
2013-04-17 14:21:45 +0200

    Some more code for the talk

--- build.sbt
+++ build.sbt
@@ -7,5 +7,6 @@ scalaVersion := "2.10.0"
 libraryDependencies ++= Seq(
   "org.scalaz"  %% "scalaz-core"   % "7.0.0-RC2",
   "org.scalaz"  %% "scalaz-effect" % "7.0.0-RC2",
+  "org.scalaz"  %% "scalaz-scalacheck-binding" % "7.0.0-RC2",
   "machines"    %% "machines"      % "0.1-SNAPSHOT"
 )
---
+++ src/main/scala/Slides.scala
@@ -0,0 +1,42 @@
+import scalaz._, Scalaz._
+import Free.Trampoline, Trampoline._
+
+object Slides {
+  def even(n: Int): Boolean =
+    if (n == 0) true
+    else odd(n - 1)
+  def odd(n: Int): Boolean =
+    if (n == 0) false
+    else even(n - 1)
+
+  def evenTr(n: Int): Trampoline[Boolean] =
+    if (n == 0) done(true)
+    else suspend(oddTr(n - 1))
+  def oddTr(n: Int): Trampoline[Boolean] =
+    if (n == 0) done(false)
+    else suspend(evenTr(n - 1))
+
+  def fibRec(n: Int): Int =
+    if (n < 2) n else fibRec(n - 1) + fibRec(n - 2)
+  def fibTailrec(n: Int) = {
+    def loop(n: Int, next: Int, result: Int): Int = n match {
+      case 0 => result
+      case _ => loop(n - 1, next + result, next)
+    }
+    loop(n, 1, 0)
+  }
+  def fibTramp(n: Int): Trampoline[Int] =
+    if (n < 2) done(n) else suspend {
+      for {
+        i <- fibTramp(n - 1)
+        j <- fibTramp(n - 2)
+      } yield i + j
+    }
+
+  import scalaz.scalacheck.ScalazProperties._
+  def checkInt =
+    semigroup.laws[Int]
+
+  def checkStr = 
+    monoid.laws[String]
+}
